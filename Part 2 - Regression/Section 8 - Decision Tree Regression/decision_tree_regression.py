5a7299d09c2d6c8d8d5d2d5ad710c6a359a8fdf6
Ryan Breen <ryan@ryanbreen.com>
2017-09-30 23:35:01 -0400

    Decision tree in python.

---
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -0,0 +1,29 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2:3].values
+
+# Fitting polynomial
+from sklearn.tree import DecisionTreeRegressor
+reg = DecisionTreeRegressor(random_state = 0)
+reg.fit(X, y);
+
+# Visualize poly smooth
+X_grid = np.arange(min(X), max(X), 0.001)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, reg.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Predict with linear
+y_pred = reg.predict(6.5)
