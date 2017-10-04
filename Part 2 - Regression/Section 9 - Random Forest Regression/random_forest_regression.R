bcb358c4b429871aa87b74ed094989e7d6d79c7f
Ryan Breen <ryan@ryanbreen.com>
2017-10-03 22:38:32 -0400

    Random forest in python.

---
+++ Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.R
@@ -0,0 +1,26 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Create regressor
+#poly_reg = lm(formula = Salary ~ ., data = dataset)
+#install.packages('rpart')
+library(rpart)
+reg = rpart(formula = Salary ~ ., data = dataset, control = rpart.control(minsplit = 1))
+
+# Predict svr
+y_poly = predict(reg, data.frame(Level = 6.5))
+
+# Visualize svr
+
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), color = 'red') +
+  geom_line(aes(x = x_grid, y = predict(reg, newdata = data.frame(Level = x_grid))), color = 'blue') +
+  ggtitle('Truth or Bluff (Regressor)') +
+  xlab('Level') +
+  ylab('Salary')
